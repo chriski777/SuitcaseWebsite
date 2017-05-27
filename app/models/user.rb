2bebb0981e255c49d6d44c1d4cdeab09670e65f1
Chris <chriski777@berkeley.edu>
2017-05-27 01:56:58 -0700

    Added uniqueness validation to user class along with case sensitivity & other features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	#checks if the name attribute is present
+	#essentially all validations must be passed in order for a User instance to be valid.
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  #Disallow double dots in email names
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  				format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
