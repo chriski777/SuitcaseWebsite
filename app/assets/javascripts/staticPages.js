181e5be42deac2b9f53ddc72a82433db99046793
Chris <chriski777@berkeley.edu>
2017-08-18 16:06:03 -0700

    Made changes to css

--- app/assets/javascripts/staticPages.js
+++ app/assets/javascripts/staticPages.js
@@ -6,4 +6,22 @@ $( document ).on('turbolinks:load', function() {
 		console.log('we made it');
 	}
 	$body.addClass('loaded');
+  //to enable smooth scrolling upon anchor click
+  $("a").on('click', function(event) {
+    //this.hash must override default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to implement smooth scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){ 
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } 
+  });
 });
+
